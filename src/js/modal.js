2751ea8778acb2ae7f13b57bbd82dbb449097936
Louella mengru Huang <932400868@cit3-501m-08.local>
2019-04-10 10:48:13 +1200

    added modal js file, modal info

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,23 @@
+//Work from Louella
 
+// Inset Mini map
+let userLocation = [-36.8977931, 174.7854973];
+let miniMap = L.map('jsMiniMap').setView(userLocation, 17);
+
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGhhbHl4OTAiLCJhIjoiY2o2YjdrZHRlMWJmYjJybDd2cW1rYnVnNSJ9.j_DQLfixHfhioVjH6qmqkw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
+}).addTo(miniMap);
+
+// radius
+L.circle(center, {
+		radius: 16500,
+		color: 'green',
+		weight: 1,
+		fill: true
+	}).addTo(miniMap);
+
+// disable scrolling in mini map
