c81d1719e0da0f16e6a4c521df4107bed78e5ec9
Louella mengru Huang <932400868@cit3-501m-08.local>
2019-04-12 10:05:59 +1200

    added louella's foursquare auth

--- src/js/auth.js
+++ src/js/auth.js
@@ -2,10 +2,10 @@
 const client_id = '&client_id=' + 'Q5PQQNTJS4UGM10I1RWODUVC4XS2PBULBDYZKFLAFECJ4ZIM';
 const client_secret = '&client_secret=' + '4TMTXMB4LL35O3GR1QXONNBAJ5OCGZUO5HLWAQQGSHHPZZV2';
 
-// Foursquare API Credentials - Nikita
+// // Foursquare API Credentials - Nikita
 // const client_id = '&client_id=' + 'Q5PQQNTJS4UGM10I1RWODUVC4XS2PBULBDYZKFLAFECJ4ZIM';
 // const client_secret = '&client_secret=' + '4TMTXMB4LL35O3GR1QXONNBAJ5OCGZUO5HLWAQQGSHHPZZV2';
 
-// Foursquare API Credentials - Louella
-// const client_id = '&client_id=' + '';
-// const client_secret = '&client_secret=' + '';
+// // Foursquare API Credentials - Louella
+// const client_id = '&client_id=' + 'JSBCLUKSAG3BD0D1BOIRNAEMKWTQY0P401LJCYETQQXBS3W0';
+// const client_secret = '&client_secret=' + 'HSRPRSP1SQJWTG3NN2EOBPCASHO52SBOU4ZPFXOJPZYIL3DK';
