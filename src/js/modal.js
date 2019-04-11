0491974461d0bb17568136fdf5e18f55c2782085
Louella <louella.huang@gmail.com>
2019-04-11 23:37:28 +1200

    Fixed map inside modal
    Moved modal into modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,29 +1,37 @@
 //Work from Louella
 
 // foursquare Oauth
-const version = '?v=20170901';
-const clientid = '&client_id=JSBCLUKSAG3BD0D1BOIRNAEMKWTQY0P401LJCYETQQXBS3W0';
-const clientSecret = '&client_secret=HSRPRSP1SQJWTG3NN2EOBPCASHO52SBOU4ZPFXOJPZYIL3DK';
-const key = version + clientid + clientSecret;
+// const version = '?v=20170901';
+// const clientid = '&client_id=JSBCLUKSAG3BD0D1BOIRNAEMKWTQY0P401LJCYETQQXBS3W0';
+// const clientSecret = '&client_secret=HSRPRSP1SQJWTG3NN2EOBPCASHO52SBOU4ZPFXOJPZYIL3DK';
+// const key = version + clientid + clientSecret;
 
 // const venueId = '59a45921351e3d43b07028b5';
 // const venueUrl = 'https://api.foursquare.com/v2/venues/' + venueId;
 
-// Inset Mini map
-let userLocation = [-36.8977931, 174.7854973];
-let miniMap = L.map('jsMiniMap').setView(userLocation, 17);
 
+// Load modal
+function modal() {
+    $('#modalCenter').on('shown.bs.modal', function () {
+        // Insert Mini map
+        // https://leafletjs.com/reference-1.0.0.html
+        let userLocation = [-36.8977931, 174.7854973];
+        let miniMap = L.map('jsMiniMap', {
+            scrollWheelZoom: false
+        }).setView(userLocation, 17);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
-}).addTo(miniMap);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
+        }).addTo(miniMap);
+    });
+}
 
+// Load Carousel
+$('.carousel').carousel();
 
-
-// disable scrolling in mini map
 
 // Ajax request to a specific venue
 // let venueUrl = 'https://api.foursquare.com/v2/venues/explore' + key + '&ll=-36.8446152873055 , 174.76662397384644' + '&radius=60';
