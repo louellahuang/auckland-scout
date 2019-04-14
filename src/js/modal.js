c1e880809d69890e6bff9ecd0c9d2a058d295145
Louella mengru Huang <932400868@cit3-501m-08.local>
2019-04-15 10:05:13 +1200

    changed modal body content

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,22 +16,10 @@ const jsDirections = document.getElementById('jsDirections');
 // Load modal
 function modal() {
     $('#modalCenter').on('shown.bs.modal', function () {
-    //Add venue details through API call
-    showVenueDetails();
+        //Add venue details through API call
+        showVenueDetails();
 
-    // Insert Mini map with user location
-    // https://leafletjs.com/reference-1.0.0.html
-    let userLocation = [-36.8977931, 174.7854973];
-    let miniMap = L.map('jsMiniMap', {
-        scrollWheelZoom: false
-    }).setView(userLocation, 17);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
-    }).addTo(miniMap);
-});
+    });
 }
 
 // On Click of each marker - make AJAX request for Markers Venue data
@@ -49,13 +37,17 @@ function showVenueDetails() {
             // Expose data of VENUE we just CLICKED
             console.log(res);
 
-            $('.modal-title').text(res.response.venue.name);
+            //For Each Venue
+
+            // Modal title
+            jsModalLongTitle.innerHTML(res.response.venue.name);
 
             // Clear/reset modal body copy so it's empty, before we add new data
             $('.modal-body').empty();
 
+            // Venue description
             if (res.response.venue.description !== undefined) {
-                $('.modal-body').append('<p class="description">' + res.response.venue.description + '</p>');
+                jsVenueDescription.innerHTML(res.response.venue.description);
             }
 
             //Construct modal/
@@ -65,19 +57,33 @@ function showVenueDetails() {
                 $('<img src=' + photoPrefix + '100x100' + photoSuffix + '>').appendTo('.modal-body');
             }
 
+            // Add Phone
             if (res.response.venue.contact.phone !== undefined) {
                 $('.modal-body').append('<p class="phone"><span class="bold">Phone:</span> ' + res.response.venue.contact.phone + '</p>');
             }
 
-            // add address
+            // Add address
             if (res.response.venue.location.address !== undefined) {
                 jsVenueWebsite.innerHTML(res.response.venue.location.address + ', ' + res.response.venue.location.city)
             }
-            
+
+            // Insert Mini map with user location
+            // https://leafletjs.com/reference-1.0.0.html
+            let userLocation = [-36.8977931, 174.7854973];
+            let miniMap = L.map('jsMiniMap', {
+                scrollWheelZoom: false
+            }).setView(userLocation, 17);
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
+            }).addTo(miniMap);
+
             // Now toggle the Modal
             $('#myModal').modal('show');
         } // End Success
 
     }); // END AJAX request for venue data
 }
-    
+
