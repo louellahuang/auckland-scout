2f8687a0a29a9f1c0adb40c865348d182387577d
nikitahoines <nikitahoines12@gmail.com>
2019-04-16 09:05:00 +1200

    adding venue id to pop up button to prepare for the function that pulls in venue details

--- src/js/main.js
+++ src/js/main.js
@@ -112,7 +112,7 @@ function createMap(v) {
         popupAnchor: [0, -36]
       });
       var marker = L.marker(venue.latlng, { icon: serviceIcon }).addTo(mymap);
-      marker.bindPopup('<div>' + venue.name + '</div>' + '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCenter">' + 'Explore' + '</button>')
+      marker.bindPopup('<div>' + venue.name + '</div>' + '<button type="button" id="'+ venue.id +'" class="btn btn-primary popupButton" data-toggle="modal" data-target="#modalCenter">' + 'Explore' + '</button>')
 
     } catch (error) {
       console.log(error);
