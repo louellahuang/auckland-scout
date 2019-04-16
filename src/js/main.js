dd991c80fac8f8ada882c5b12d4d09dcedd5eac2
Louella mengru Huang <932400868@cit3-501m-08.local>
2019-04-17 11:24:00 +1200

    added map into modal according to clicked venue

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,8 @@ const jsCatParks = document.getElementById('jsCatParks');
 const jsCatBeaches = document.getElementById('jsCatBeaches');
 const jsCatLookouts = document.getElementById('jsCatLookouts');
 // Modal identifiers
+var modalContentContainer = document.getElementById('modalContentContainer');
+var modalContentFooter = document.getElementById('modalContentFooter');
 const jsPopUpButton = document.getElementById('#modalCenter');
 const jsModalLongTitle = document.getElementById('jsModalLongTitle');
 const jsVenueImg = document.getElementById('jsVenueImg');
@@ -165,63 +167,83 @@ function fetchVenue(uri) {
 }
 
 function venueDetails(res) {
-
-  console.log(res);
-  // Modal title
-  jsModalLongTitle.innerHTML = res.venue.name;
-
-  // // Clear/reset modal body copy so it's empty, before we add new data
-  // if ( ) {
-  // $('.modal-body').empty();
-  // $('.modal-footer').empty();
-  // }
-
-
-  // // Venue description
-  // if (res.venue.description !== undefined) {
-  //   jsVenueDescription.innerHTML = res.venue.description;
-  // }
-
-  //Construct modal image/
-  if (res.venue.photos.groups.length > 0) {
-    const photoPrefix = res.venue.bestPhoto.prefix;
-    const photoSuffix = res.venue.bestPhoto.suffix;
-    jsVenueImg.src = photoPrefix + '720x720' + photoSuffix;
-  }
-
-  // Add website
-    if (res.venue.shortUrl !== undefined) {
-      jsVenueWebsite.href = res.venue.shortUrl;
-  }
-
-  
-  console.log('hello')
   window.fake = res;
-
+  // Modal title
+  const title = res.venue.name;
+  // Venue description
+  if (res.venue.description !== undefined) {
+    var description = res.venue.description;
+  } else {
+    var description = 'This venue does not have a description';
+  }
+  //Construct modal image/
+  if (res.venue.bestPhoto !== undefined) {
+    var photoPrefix = res.venue.bestPhoto.prefix;
+    var photoSuffix = res.venue.bestPhoto.suffix;
+    var venueImage = photoPrefix + '720x720' + photoSuffix;
+  } else {
+    var venueImage = 'src/img/dog-3.jpg';
+  }
+  // Add website
+  if (res.venue.shortUrl !== undefined) {
+    var website = res.venue.shortUrl;
+  }
   // Add address
   if (res.venue.location.address !== undefined) {
-    jsVenueWebsite.innerHTML = res.venue.location.address + ', ' + res.venue.location.city;
+    var address = res.venue.location.address + ', ' + res.venue.location.city;
+  } else {
+    var address = 'This venue does not have an address';
   }
 
-  // Insert Mini map with user location https://leafletjs.com/reference-1.0.0.html
-  // remove previously loaded map first
 
+  // remove previously loaded map first
   // if (miniMap) {
   //   miniMap.remove();
   // }
-  // let userLocation = [-36.8977931, 174.7854973];
-  // let miniMap = L.map('jsMiniMap', {
-  //   scrollWheelZoom: false
-  // }).setView(userLocation, 17);
-  // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //   maxZoom: 18,
-  //   id: 'mapbox.streets',
-  //   accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
-  // }).addTo(miniMap);
+
+  // Adding Mini Map
+  // Insert Mini map with user location https://leafletjs.com/reference-1.0.0.html
+  let venueLocation = [res.venue.location.lat, res.venue.location.lng];
+  let miniMap = L.map('jsMiniMap', {
+    scrollWheelZoom: false
+  }).setView(venueLocation, 17);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibmlraXRhaG9pbmVzIiwiYSI6ImNqc203cHN5NDEwaGg0OXBpYnE0aXhhZmYifQ.58l8dUZg4uiFn7BYnZCJFA'
+  }).addTo(miniMap);
+
+
+
+  // Directions
+  const directions = 'directions';
+
+  createModalContent(title, venueImage, description, address, website, directions);
+};
+
+function createModalContent(title, venueImage, description, address, website, directions) {
+
+
+  modalContentContainer.innerHTML = `
+  <div class="modal-header">
+    <h5 class="modal-title" id="jsModalLongTitle">${title}</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <div class="venue-item">
+      <img class="d-block w-100" id="jsVenueImg" src="${venueImage}" alt="modal-example">
+    </div>
+    <p id="jsVenueDescription">${description}</p>
+    <p class="jsVenueAddress">${address}</p>
+    <a class="pr-5" id="jsVenueWebsite" href="${website}" target="_blank">website link</a>
+    <a href="${directions}" class="btn btn-primary" id="jsDirections" target="_blank">Get Directions</a>
+  </div>
+  `;
 
   // Now toggle the Modal
   $('#modalCenter').modal('show');
 
-
-};
+}
