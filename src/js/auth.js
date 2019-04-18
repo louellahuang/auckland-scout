8d899c32c863ae5820458c5036288ec69d7496b5
Louella <louella.huang@gmail.com>
2019-04-18 15:15:16 +1200

    added new keys at auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -7,5 +7,9 @@
 // const client_secret = '&client_secret=' + 'UIK2BOJFZ1OEZJRBIMW35SOGRB1LFETJVWICBAVBE4JJVCJP';
 
 // // Foursquare API Credentials - Louella
-const client_id = '&client_id=' + 'JSBCLUKSAG3BD0D1BOIRNAEMKWTQY0P401LJCYETQQXBS3W0';
-const client_secret = '&client_secret=' + 'HSRPRSP1SQJWTG3NN2EOBPCASHO52SBOU4ZPFXOJPZYIL3DK';
+// const client_id = '&client_id=' + 'JSBCLUKSAG3BD0D1BOIRNAEMKWTQY0P401LJCYETQQXBS3W0';
+// const client_secret = '&client_secret=' + 'HSRPRSP1SQJWTG3NN2EOBPCASHO52SBOU4ZPFXOJPZYIL3DK';
+
+// // Foursquare API Credentials - John Smith t1557976@nwytg.net yoobee12345
+const client_id = '&client_id=' + 'NTVGVUVVSZ5YDPGPAQF5STMX4NC25NYT1S3X1O5MEOG3GGYG';
+const client_secret = '&client_secret=' + 'BEWE5CHPVGE31A1WIDLOZBEDO1K2MFP5I4KCU1EMW3EMG02T';
